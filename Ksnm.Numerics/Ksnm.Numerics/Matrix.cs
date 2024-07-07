f0bd17cb0bb4dabfdb36e8801878edf7c1aa6705
笠浪貴大 <dev.kacha@gmail.com>
2024-07-08 00:05:12 +0900

    Matrixクラス追加

---
+++ Ksnm.Numerics/Ksnm.Numerics/Matrix.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ksnm.Numerics
+{
+    public class Matrix<TValue> :
+        IAdditionOperators<Matrix<TValue>, Matrix<TValue>, Matrix<TValue>>,
+        IAdditiveIdentity<Matrix<TValue>, Matrix<TValue>>,
+        ISubtractionOperators<Matrix<TValue>, Matrix<TValue>, Matrix<TValue>>,
+        IMultiplyOperators<Matrix<TValue>, Matrix<TValue>, Matrix<TValue>>,
+        IMultiplicativeIdentity<Matrix<TValue>, Matrix<TValue>>
+        where TValue : INumber<TValue>
+    {
+        #region 定数
+        public static Matrix<TValue> AdditiveIdentity => throw new NotImplementedException();
+
+        public static Matrix<TValue> MultiplicativeIdentity => throw new NotImplementedException();
+
+        #endregion 定数
+        /// <summary>
+        /// _arrayに
+        /// </summary>
+        public enum Status
+        {
+            None = 0,
+            AdditiveIdentity,
+        }
+
+        #region フィールド
+        /// <summary>
+        /// 1次元配列
+        /// </summary>
+        private TValue[] _array = new TValue[0];
+        #endregion フィールド
+
+        #region プロパティ
+        Status status;
+        public int RowLength = 0;
+        public int ColumnLength = 0;
+        public int ArrayLength => RowLength * ColumnLength;
+        public TValue this[int row, int column]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                var index = GetIndex(row, column);
+                return _array[index];
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                var index = GetIndex(row, column);
+                _array[index] = value;
+            }
+        }
+        #endregion プロパティ
+
+        #region コンストラクタ
+        public Matrix() { }
+
+        public Matrix(int rowLength, int columnLength)
+        {
+            RowLength = rowLength;
+            ColumnLength = columnLength;
+            _array = new TValue[rowLength * columnLength];
+        }
+
+        public Matrix(Matrix<TValue> other) : this(other.RowLength, other.ColumnLength)
+        {
+            Array.Copy(other._array, _array, ArrayLength);
+        }
+
+        public Matrix(Matrix4x4 other)
+        {
+            _array = new TValue[4 * 4];
+            for (int r = 0; r < 4; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    var index = GetIndex(r, c);
+                    _array[index] = TValue.CreateChecked(other[r, c]);
+                }
+            }
+        }
+        #endregion コンストラクタ
+
+        #region Get
+        public int GetIndex(int row, int column)
+        {
+            return row * ColumnLength + column;
+        }
+        public IEnumerable<TValue> GetColumnItems(int column)
+        {
+            for (int r = 0; r < RowLength; r++)
+            {
+                var index = GetIndex(r, column);
+                yield return _array[index];
+            }
+        }
+        public IEnumerable<TValue> GetRowItems(int row)
+        {
+            for (int c = 0; c < ColumnLength; c++)
+            {
+                var index = GetIndex(row, c);
+                yield return _array[index];
+            }
+        }
+        #endregion Get
+
+        #region operators
+        public static Matrix<TValue> operator +(Matrix<TValue> left, Matrix<TValue> right)
+        {
+            var rowLength = System.Math.Min(left.RowLength, right.RowLength);
+            var columnLength = System.Math.Min(left.ColumnLength, right.ColumnLength);
+            var temp = new Matrix<TValue>(rowLength, columnLength);
+
+            for (int r = 0; r < rowLength; r++)
+            {
+                for (int c = 0; c < columnLength; c++)
+                {
+                    var index = temp.GetIndex(r, c);
+                    temp._array[index] = left[r, c] + right[r, c];
+                }
+            }
+            return temp;
+        }
+
+        public static Matrix<TValue> operator -(Matrix<TValue> left, Matrix<TValue> right)
+        {
+            var rowLength = System.Math.Min(left.RowLength, right.RowLength);
+            var columnLength = System.Math.Min(left.ColumnLength, right.ColumnLength);
+            var temp = new Matrix<TValue>(rowLength, columnLength);
+
+            for (int r = 0; r < rowLength; r++)
+            {
+                for (int c = 0; c < columnLength; c++)
+                {
+                    var index = temp.GetIndex(r, c);
+                    temp._array[index] = left[r, c] - right[r, c];
+                }
+            }
+            return temp;
+        }
+
+        public static Matrix<TValue> operator *(Matrix<TValue> left, Matrix<TValue> right)
+        {
+            var rowLength = left.RowLength;
+            var columnLength = right.ColumnLength;
+            var temp = new Matrix<TValue>(rowLength, columnLength);
+
+            for (int r = 0; r < rowLength; r++)
+            {
+                var leftRowItems = left.GetRowItems(r);
+                for (int c = 0; c < columnLength; c++)
+                {
+                    var rightColumnItems = right.GetColumnItems(c);
+                    var count = Min(leftRowItems.Count(), rightColumnItems.Count());
+                    TValue tempValue = TValue.Zero;
+                    for (int i = 0; i < count; i++)
+                    {
+                        tempValue += leftRowItems.ElementAt(i) * rightColumnItems.ElementAt(i);
+                    }
+                    var index = temp.GetIndex(r, c);
+                    temp._array[index] = tempValue;
+                }
+            }
+            return temp;
+        }
+        #endregion operators
+
+        #region object
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("{");
+            for (int r = 0; ;)
+            {
+                stringBuilder.Append("{");
+                for (int c = 0; ;)
+                {
+                    var index = GetIndex(r, c);
+                    stringBuilder.Append(_array[index].ToString());
+                    // 次へ
+                    c++;
+                    if (c < ColumnLength)
+                    {
+                        stringBuilder.Append(",");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                stringBuilder.Append("}");
+                r++;
+                if (r < RowLength)
+                {
+                    stringBuilder.Append(",");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return Equals((Matrix<TValue>)obj);
+        }
+        public bool Equals(Matrix<TValue> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if (_array.Length != other._array.Length)
+            {
+                return false;
+            }
+            return _array.SequenceEqual(other._array);
+        }
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            foreach (var item in _array)
+            {
+                hashCode = hashCode ^ item.GetHashCode();
+            }
+            return hashCode;
+        }
+        #endregion object
+
+        #region private
+        private static int Min(int a, int b)
+        {
+            return System.Math.Min(a, b);
+        }
+        #endregion private
+    }
+}
---
+++ Ksnm.Numerics/TestProject/MatrixTests.cs
@@ -0,0 +1,121 @@
+﻿using Ksnm.Numerics;
+using System.Numerics;
+
+namespace TestProject
+{
+    // コードを再利用するためのエイリアスを定義
+    using Int8 = sbyte;
+    using UInt8 = byte;
+    using Float16 = Half;
+    using Float32 = float;
+    using Float64 = double;
+    [TestClass()]
+    public class MatrixTests
+    {
+        [TestMethod()]
+        public void ConstantTest()
+        {
+        }
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Matrix<int> expected = new Matrix<int>(2, 2);
+            Matrix<int> actual = new Matrix<int>(expected);
+            Assert.AreEqual(expected, actual);
+
+            expected[0, 0] = 1;
+            actual[0, 0] = 1;
+            Assert.AreEqual(actual, expected);
+
+            expected[1, 1] = 1;
+            actual[1, 1] = 1;
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod()]
+        public void CastTest()
+        {
+        }
+
+        [TestMethod()]
+        public void OperationsTest1()
+        {
+        }
+
+        [TestMethod()]
+        public void OperationsTest2()
+        {
+            Matrix<int> expected = new Matrix<int>(2, 2);
+            Matrix<int> actual = new Matrix<int>(expected);
+            Matrix<int> a = new Matrix<int>(expected);
+            Matrix<int> b = new Matrix<int>(expected);
+            // +
+            {
+                expected[0, 0] = 34;
+                expected[0, 1] = 68;
+                expected[1, 0] = 136;
+                expected[1, 1] = 272;
+
+                a[0, 0] = 2;
+                a[0, 1] = 4;
+                a[1, 0] = 8;
+                a[1, 1] = 16;
+
+                b[0, 0] = 32;
+                b[0, 1] = 64;
+                b[1, 0] = 128;
+                b[1, 1] = 256;
+                actual = a + b;
+                Assert.AreEqual(expected, actual);
+            }
+            // -
+            {
+                expected[0, 0] = 30;
+                expected[0, 1] = 60;
+                expected[1, 0] = 120;
+                expected[1, 1] = 240;
+
+                a[0, 0] = 2;
+                a[0, 1] = 4;
+                a[1, 0] = 8;
+                a[1, 1] = 16;
+
+                b[0, 0] = 32;
+                b[0, 1] = 64;
+                b[1, 0] = 128;
+                b[1, 1] = 256;
+                actual = b - a;
+                Assert.AreEqual(expected, actual);
+            }
+            // *
+            {
+                expected[0, 0] = 32;
+                expected[0, 1] = 51;
+                expected[1, 0] = 46;
+                expected[1, 1] = 75;
+
+                a[0, 0] = 2;
+                a[0, 1] = 5;
+                a[1, 0] = 4;
+                a[1, 1] = 7;
+
+                b[0, 0] = 1;
+                b[0, 1] = 3;
+                b[1, 0] = 6;
+                b[1, 1] = 9;
+                actual = a*b;
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            for (int i = -10; i < 10; i++)
+            {
+                var a = new Matrix<int>(i, i);
+                var b = new Matrix<int>(i, i);
+                Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+            }
+        }
+    }
+}
